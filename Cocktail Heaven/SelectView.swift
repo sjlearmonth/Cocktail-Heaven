f09392c6eaee37bbea3f50726ea9e122b211a7fe
Stephen J Learmonth <stephen.j.learmonth@gmail.com>
2021-10-19 17:34:26 +0100

    Buttons width extended

--- Cocktail Heaven/SelectView.swift
+++ Cocktail Heaven/SelectView.swift
@@ -9,16 +9,45 @@ import SwiftUI
 
 struct SelectView: View {
     @State var resultsIsSelected = false
+    @State var pickerIsSelected = false
     var body: some View {
         ZStack {
             Color(.systemTeal)
                 .edgesIgnoringSafeArea(.all)
-            VStack {
-                NavigationLink(isActive: $resultsIsSelected) {
-                    ResultsView()
+            VStack(spacing: 30) {
+                NavigationLink(isActive: $pickerIsSelected) {
+                    List {
+                        Text("A")
+                        Text("B")
+                        Text("C")
+                        Text("D")
+                        Text("E")
+                        Text("F")
+                        Text("G")
+                        Text("H")
+                        Text("I")
+                        Text("J")
+                        Text("K")
+                        Text("L")
+                        Text("M")
+                        Text("N")
+                        Text("O")
+                        Text("P")
+                        Text("Q")
+                        Text("R")
+                        Text("S")
+                        Text("T")
+                        Text("U")
+                        Text("V")
+                        Text("W")
+                        Text("X")
+                        Text("Y")
+                        Text("Z")
+                    }
+                    
                 } label: {
                     Button {
-                        resultsIsSelected = true
+                        pickerIsSelected = true
                     } label: {
                         Text("Search by first letter")
                             .foregroundColor(.white)
@@ -26,6 +55,7 @@ struct SelectView: View {
                             .padding()
                             .navigationTitle("Select Search Option")
                             .navigationBarTitleDisplayMode(.inline)
+                            .frame(width: UIScreen.main.bounds.width - 20.0)
                     }
                     .background(.blue)
                     .clipShape(RoundedRectangle(cornerRadius: 30.0))
@@ -41,6 +71,7 @@ struct SelectView: View {
                             .foregroundColor(.white)
                             .font(.system(size: 25.0))
                             .padding()
+                            .frame(width: UIScreen.main.bounds.width - 20.0)
                     }
                     .background(.blue)
                     .clipShape(RoundedRectangle(cornerRadius: 30.0))
@@ -56,6 +87,23 @@ struct SelectView: View {
                             .foregroundColor(.white)
                             .font(.system(size: 25.0))
                             .padding()
+                            .frame(width: UIScreen.main.bounds.width - 20.0)
+                    }
+                    .background(.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 30.0))
+                }
+                
+                NavigationLink(isActive: $resultsIsSelected) {
+                    ResultsView()
+                } label: {
+                    Button {
+                        resultsIsSelected = true
+                    } label: {
+                        Text("Search by alcohol content")
+                            .foregroundColor(.white)
+                            .font(.system(size: 25.0))
+                            .padding()
+                            .frame(width: UIScreen.main.bounds.width - 20.0)
                     }
                     .background(.blue)
                     .clipShape(RoundedRectangle(cornerRadius: 30.0))
